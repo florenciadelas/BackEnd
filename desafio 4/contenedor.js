061229af17f716dc5eb1ff3cf4aaac01aa63432f
florenciadelas <florenciadelas@hotmail.com>
2022-09-08 18:11:26 -0300

    Desafio 4 v_2

--- desafio 4/contenedor.js
+++ desafio 4/contenedor.js
@@ -1,7 +1,7 @@
 class Container {
-    constructor(array) {
-      this.array = array;
-    }
+  constructor(array) {
+    this.array = array;
+  }
   async save(objecto) {
     try {
       const productFound = this.array.find(
@@ -21,66 +21,79 @@ class Container {
       console.error(`Error no se encontro el producto`);
     }
   }
-  
-    async getById(idProducto) {
-      try {
-        const idOk = this.array.find(({ id }) => id == idProducto);
-        if (idOk) {
-          console.log(`Se encontro correctamente`);
-          return idOk;
-        } else {
-          console.log("No se han encontrado");
-        }
-      } catch (error) {
-        console.error(`Se produjo un error`);
+
+  async getById(idProducto) {
+    try {
+      const idOk = this.array.find(({
+        id
+      }) => id == idProducto);
+      if (idOk) {
+        console.log(`Se encontro correctamente`);
+        return idOk;
+      } else {
+        console.log("No se han encontrado");
       }
-    }
-  
-    async getAll() {
-      try {
-        return this.array;
-      } catch (error) {
-        console.error(`Se ha producido un error`);
-      }
-    }
-  
-    updateById(idProducto, objecto) {
-      try {
-        const idOk = this.array.filter(({ id }) => id != idProducto);
-        const prodEncontrado = this.array.find(({ id }) => id == idProducto);
-  
-        if (prodEncontrado) {
-          const prodEncontrado = { ...objecto, id: idProducto};
-          idOk.push(prodEncontrado);
-          this.array = idOk;
-          console.log(`Se realizo el cambio correctamente`, prodEncontrado);
-          return prodEncontrado;
-        } else {
-          return null;
-        }
-      } catch (error) {
-        console.error(`Se produjo un error`);
-      }
-    }
-    
-    async deleteById(idEntered) {
-      try {
-        const leakedID = this.array.filter(({ id }) => id != idEntered);
-        const idFound = this.array.find(({ id }) => id == idEntered);
-  
-        if (idFound) {
-          console.log(
-            `Se ha eliminado el objeto con id:${idEntered} >> [[${idFound.title}]]`
-          );
-          this.array = leakedID
-          return idFound;
-        } else {
-          console.log(`No se ha encontrado el objeto con id: ${idEntered}`);
-        }
-      } catch (error) {
-        console.error(`Se ha producido un error en deleteById: ${error}`);
-      }
+    } catch (error) {
+      console.error(`Se produjo un error`);
     }
   }
-  
-  module.exports = Container;
+
+  async getAll() {
+    try {
+      return this.array;
+    } catch (error) {
+      console.error(`Se ha producido un error`);
+    }
+  }
+
+  updateById(idProducto, objecto) {
+    try {
+      const idOk = this.array.filter(({
+        id
+      }) => id != idProducto);
+      const prodEncontrado = this.array.find(({
+        id
+      }) => id == idProducto);
+
+      if (prodEncontrado) {
+        const prodEncontrado = {
+          ...objecto,
+          id: idProducto
+        };
+        idOk.push(prodEncontrado);
+        this.array = idOk;
+        console.log(`Se realizo el cambio correctamente`, prodEncontrado);
+        return prodEncontrado;
+      } else {
+        return null;
+      }
+    } catch (error) {
+      console.error(`Se produjo un error`);
+    }
+  }
+
+  async deleteById(idProducto) {
+    try {
+      const idOk = this.array.filter(({
+        id
+      }) => id != idProducto);
+      const prodEncontrado = this.array.find(({
+        id
+      }) => id == idProducto);
+
+      if (prodEncontrado) {
+        console.log(
+          `Se ha eliminado el producto`
+        );
+        this.array = idOk
+        return prodEncontrado;
+      } else {
+        console.log(`No se ha encontrado el producto}`);
+      }
+    } catch (error) {
+      console.error(`Se ha producido un error`);
+    }
+  }
+}
+
+module.exports = Container;
--- desafio 4/main.js
+++ desafio 4/main.js
@@ -1,26 +1,29 @@
 const express = require("express");
 const app = express();
-
 const productos = [];
 const Container = require("./Contenedor");
 const contenedor = new Container(productos);
 
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({
+  extended: false
+}));
 app.use(express.json());
 app.use("/", express.static(__dirname + "/public"));
 
 
 app.get("/api/productos", async (req, res) => {
-  try{
-  res.send(contenedor.getAll());
-}catch{
-  res.status(200).json(data);
-}});
-
+  try {
+    res.send(contenedor.getAll());
+  } catch {
+    res.status(200).json(data);
+  }
+});
 
 app.get("/api/productos/:id", async (req, res) => {
   try {
-    const { id } = req.params;
+    const {
+      id
+    } = req.params;
     res.send(contenedor.getById(parseInt(id)));
   } catch (err) {
     res.status(404).send(err);
@@ -29,16 +32,28 @@ app.get("/api/productos/:id", async (req, res) => {
 
 
 app.post("/api/productos", async (req, res) => {
-  const { nombre, precio, url } = req.body;
-  const data = await contenedor.save({ nombre, precio, url });
-  data == null
-    ? res.status(500).json({ message: `Error en la caga` })
-    : res.status(200).json(data);
+  const {
+    nombre,
+    precio,
+    url
+  } = req.body;
+  const data = await contenedor.save({
+    nombre,
+    precio,
+    url
+  });
+  data == null ?
+    res.status(500).json({
+      message: `Error en la caga`
+    }) :
+    res.status(200).json(data);
 });
 
 app.put("/api/productos/:id", (req, res) => {
   try {
-    const { id } = req.params;
+    const {
+      id
+    } = req.params;
     const prodNuevo = req.body;
     const idInt = parseInt(id);
     res.send(constructor.updateById(idInt, prodNuevo));
@@ -49,7 +64,9 @@ app.put("/api/productos/:id", (req, res) => {
 
 app.delete("/api/productos/:id", (req, res) => {
   try {
-    const { id } = req.params;
+    const {
+      id
+    } = req.params;
     res.send(contenedor.deleteById(parseInt(id)));
   } catch (err) {
     res.status(404).send(err.msg);
